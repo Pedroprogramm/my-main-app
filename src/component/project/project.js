489355a7c8b76a24c506166ed3695c5bc9c4c373
stanislau <stas0rar@gmail.com>
2023-08-14 15:58:58 +0300

    css

--- src/component/project/project.js
+++ src/component/project/project.js
@@ -7,9 +7,11 @@ import Snowfall from 'react-snowfall'
 
 
 const OurProject = () => {
-  const [nameProject, setNameProject] = useState('Develop');
+  const [nameProject, setNameProject] = useState('Develop'),
+  [resourcesLoaded, setResourcesLoaded] = useState(false),
+  [allProject, setAllProject] = useState(false);
 
-  
+
   const updateMousePosition = (e) => {
       const y = e.clientY - (window.innerHeight * 0.5) ,
       x = e.clientX - (window.innerWidth * 0.5) ;
@@ -17,6 +19,11 @@ const OurProject = () => {
       document.documentElement.style.setProperty('--mouse-x', (`${x}deg`) );
       document.documentElement.style.setProperty('--mouse-y', (`${y}deg`));
   }
+
+  useEffect(() => {
+      setResourcesLoaded(true);
+  }, []);
+  
 
   useEffect(() => {
     window.addEventListener('mousemove', updateMousePosition);
@@ -39,7 +46,7 @@ const OurProject = () => {
   };
 
   const onLoadProject = () => {
-    // Your logic for loading more projects
+    setAllProject(true);
   };
 
   const setMain = () => {
@@ -62,13 +69,24 @@ const OurProject = () => {
                   <div className='develop-sec-layer1 img-props'></div>
                   <Snowfall />
                   <div className='develop-sec-layer2 img-props'></div>
+                  <div className='develop-sec-blur circle-blur-1'></div>
+                  <div className='develop-sec-blur circle-blur-2'></div>
+                  <div className='develop-sec-blur circle-blur-3'></div>
+                  <div className='develop-sec-blur circle-blur-4'></div>
+                  <div className='develop-sec-blur circle-blur-5'></div>
                   <div className='develop-sec-layer3 img-props'></div>
                   <div className='develop-sec-layer3-btn img-props'></div>
                 </div>
               </div>
           </div>
+          { allProject ? <h1> ALLALLALL</h1> : 
+          <div className='load-more-container'>
+            <div onClick={onLoadProject} className='load-more'>
+              больше проектов
+            </div>
+          </div>  }
         </>
-      );
+      ) ;
     } else {
       return <h1>{nameProject}</h1>;
     }
@@ -78,26 +96,37 @@ const OurProject = () => {
     <div className='project-section'>
       <div className='project-header'>Наши проекты</div>
       <div className='project-names'>
-        <p className='project-name' data-name='Develop' onClick={() => designClick('Develop')}>
+        <p className='project-name underline-one' data-name='Develop' onClick={() => designClick('Develop')}>
           Web-разработка
         </p>
-        <p className='project-name' data-name='Design' onClick={() => designClick('Design')}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="active-check" viewBox="0 0 16 16">
+            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+          </svg>
+        <p className='project-name underline-one' data-name='Design' onClick={() => designClick('Design')}>
           Дизайн
         </p>
-        <p className='project-name' data-name='Advertising' onClick={() => designClick('Advertising')}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="active-check" viewBox="0 0 16 16">
+            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+          </svg>
+        <p className='project-name underline-one' data-name='Advertising' onClick={() => designClick('Advertising')}>
           Таргетированная реклама
         </p>
-        <p className='project-name' data-name='SMM' onClick={() => designClick('SMM')}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="active-check" viewBox="0 0 16 16">
+            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+          </svg>
+        <p className='project-name underline-one' data-name='SMM' onClick={() => designClick('SMM')}>
           SMM
         </p>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="active-check" viewBox="0 0 16 16">
+            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+          </svg>
       </div>
       <div className='project-main'>
-        {setMain()}
-        <div className='load-more-container'>
-          <div onClick={onLoadProject} className='load-more'>
-            больше проектов
-          </div>
-        </div>
+        {/* {setMain()} */}
+    {resourcesLoaded ? setMain() : <div style={{ display: 'flex' }}>
+      <div className='gray-preloader'></div>
+      <div className='gray-preloader'></div>
+    </div>}
       </div>
     </div>
   );
